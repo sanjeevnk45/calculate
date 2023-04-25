6891e98702d691e530a0c507c5fcd4791af03b94
sanjeevnk45 <sanjeevnk45@gmail.com>
2023-04-25 17:04:52 +0530

    commited

--- script.js
+++ script.js
@@ -12,18 +12,4 @@ var total = (q1 * 0.5) + (q2 * 0.5) + (q3 * 0.5) + (q4 * 0.5) + (q5 * 0.5) +(q6 
 
     document.getElementById("label").innerHTML= "ನಿಮ್ಮ ಗ್ರಾಮದಲ್ಲಿ ತಿಂಗಳಿಗೆ ಸರಾಸರಿಯಾಗಿ ಉತ್ಪಾದನೆಯಾಗುವ ತ್ಯಾಜ್ಯ  " + total  + " KG";
  }
- /*
-
- function checkTextField(id) {
-    var q1=document.getElementById("q1").value;
-    if (!q1.match(/\S/)) 
-    {
-        alert("Enter all the field");
-        return false;
-    } 
-    else 
-    {
-        return true;
-    }
-  }
-  */
+ 
